3c558d69fdb3042f3a15ded64985b59d36d79514
Grzegorz Maj <grzesiekm1992@gmail.com>
2020-02-23 15:23:26 +0100

    Initial commit
    Set up of SFML in Visual Studio 2019.
    Added background for the game, and player sprite.

---
+++ CppGameMagisterka/main.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <SFML/Graphics.hpp>
+
+#define IMG = "S:\DEV-ZONE\CppGameMagisterka\CppGameMagisterka\IMG";
+
+using namespace std;
+
+int main()
+{
+		sf::RenderWindow window(sf::VideoMode(1920, 1080), "2D Game");
+		sf::Texture background, player;
+		background.loadFromFile("IMG/Floor.jpg");
+		background.setRepeated(true);
+		player.loadFromFile("IMG/player.png");
+		player.setSmooth(true);
+
+		sf::Sprite backgroundSprite(background);
+		backgroundSprite.setTextureRect(sf::IntRect(0, 0, 2250, 2250));
+		sf::Sprite playerSprite(player);
+
+		while (window.isOpen())
+		{
+			sf::Event event;
+			while (window.pollEvent(event))
+			{
+				if (event.type == sf::Event::Closed)
+					window.close();
+			}
+
+			window.clear();
+			window.draw(backgroundSprite);
+			window.draw(playerSprite);
+			window.display();
+
+		}
+
+		return 0;
+}
